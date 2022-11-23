c2a5705504f57331e610dd92ab7f9c7b9417328a
vazirabonu [FE16] <102415721+vazirabonu23@users.noreply.github.com>
2022-11-23 14:51:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+//homework1
+
+// let firstname = "Vazirabonu"
+// let surname = "O'ralova"
+// let age = "16"
+
+// console.log("Mening ismim" + " " +firstname+ ", Familiyasi esa " + surname)
+
+
+//homewoork2
+
+// let obj = {
+//     firstname: "Vazirabonu",
+//     surname:"O'ralova",
+//     age:16,
+//     color:"green",
+// }
+// console.log(obj);
+
+
+
+//homework3
+
+// let one = prompt("Birinch sonni kiriting")
+// let two= prompt("Ikkinchi sonni kiriting")
+
+// console.log(Number(one) % Number(two))
